8c8ae297a60a7db7f0bed611b08cc73db706738e
Shorya Verma <shoryaverma54@gmail.com>
2025-06-01 18:44:06 +0530

    d

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import { FaHome } from "react-icons/fa";
 const Logo = () => {
   return (
     <a href="#home" className="flex items-center space-x-2">
-      <img src="public\logo2.png" height={200} width={100} alt="" />
+      <img src="/logo2.png" height={200} width={100} alt="Logo" />
     </a>
   );
 };
